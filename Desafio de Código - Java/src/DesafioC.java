0dcc3fe60d54ec0f6ec57aa88c414d6d8078c3f8
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-04 19:00:07 -0400

    Desafio C - Completo

--- Desafio de Código - Java/src/DesafioC.java
+++ Desafio de Código - Java/src/DesafioC.java
@@ -23,7 +23,7 @@ public class DesafioC {
 
 /*Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, conforme a fórmula:
 
-Distancia = formula da distancia entre dois pontos.
+Distancia = fórmula da distancia entre dois pontos.
 
 Entrada: O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2. 1.0 7.0
 5.0 9.0
