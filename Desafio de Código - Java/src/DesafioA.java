4903bc3501f777b8514fbc3129683476d65feb19
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-06 18:44:37 -0400

    Desafio A - Completo

--- Desafio de Código - Java/src/DesafioA.java
+++ Desafio de Código - Java/src/DesafioA.java
@@ -10,17 +10,12 @@ public class DesafioA{
         Scanner scan = new Scanner(System.in);
         
         int N = scan.nextInt();
-        String string = scan.nextLine();
-
-        //quebra string em várias substrings a partir de um caracter
-
-        String[] s = string.split(" ");
 
         int[] idade = new int[N];
 
         for (int i = 0; i < N; i++)
         {
-             idade[i] = Integer.parseInt(s[i+1]);
+            idade[i] = scan.nextInt();
         }
 
         System.out.println("Nao poderao entrar as idades: ");
