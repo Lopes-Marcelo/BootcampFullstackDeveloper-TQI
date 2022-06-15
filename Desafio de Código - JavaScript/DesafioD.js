d7455141f9b36365229800e43032370fa3823d04
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-15 18:04:22 -0400

    Desafio D - Completo

--- Desafio de Código - JavaScript/DesafioD.js
+++ Desafio de Código - JavaScript/DesafioD.js
@@ -6,16 +6,6 @@ for(let i = 0; i < array.length; i++){
     }
 }
 
-/*var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
-
-//TODO: Complete os espaços em branco com uma possível solução para o desafio
- 
-for (let i = 0; i < array.     ; i++){
-    if (                      ){
-        print(array[  ]);
-     }          
-}*/
-
 /*Desafio - O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura de dados que armazena uma coleção de dados em um bloco de memória.
 
 Dado uma array de valores {2, 3, 5, 7, 11, 13, 18, 34}, desenvolva um programa que retorne como resultado apenas os números pares dessa array, conforme exemplo abaixo. */
