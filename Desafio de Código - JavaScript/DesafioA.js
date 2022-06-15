f2d1e40db456bc329770446ea51f7504d82f64af
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-15 18:10:57 -0400

    Desafio A - Completo

--- Desafio de Código - JavaScript/DesafioA.js
+++ Desafio de Código - JavaScript/DesafioA.js
@@ -1,24 +1,18 @@
-// TODO: complete os espaços em branco com sua solução para o problema
-// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
-// Abaixo segue um exemplo de código que você pode ou não utilizar
-
 let input = gets();
+let leitorNivel = parseInt(input)
+let C = leitorNivel
 
-let qtdEntradas = parseInt(input);
-let nivelEnergia;
-
-for(let i = 0; i < qtdEntradas; i++){
-    input = gets();
-    nivelEnergia = parseInt(input);
+for (let i = 0; i < C; i++) {
     
-    if(      ){
-        print   (       )
-    }else{
-        print          )
+    input = gets()
+    let N = parseInt(input)
+  	
+  	if (N > 8000){
+    		console.log("Mais de 8000!");
+  	}	else { 
+      console.log("Inseto!");
     }
 }
-
-//Resolva o desafios nos espaços em branco
 
 /*Desafio - Daenerys é a khaleesi dos Dothraki. Juntamente com Drogon, eles vão atrás do Tyrion, para tentar dominar Westeros. Ela possui, além do seu dragãozinho, um rastreador que mede o nível de energia de qualquer ser vivo. Todos os seres com o nível menor ou igual a 8000, ela considera como se fosse um inseto. Quando passa deste valor, que foi o caso do Drogon, ela se espanta e grita “Mais de 8000”. Baseado nisso, utilize a mesma tecnologia e analise o nível de energia dos seres vivos.
 
