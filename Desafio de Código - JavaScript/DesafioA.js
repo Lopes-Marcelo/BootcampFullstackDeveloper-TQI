56b196fc12c7f9d39ebb372ab178df21dcb8df2d
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-15 16:21:42 -0400

    em andamento

---
+++ Desafio de Código - JavaScript/DesafioA.js
@@ -0,0 +1,29 @@
+// TODO: complete os espaços em branco com sua solução para o problema
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let input = gets();
+
+let qtdEntradas = parseInt(input);
+let nivelEnergia;
+
+for(let i = 0; i < qtdEntradas; i++){
+    input = gets();
+    nivelEnergia = parseInt(input);
+    
+    if(      ){
+        print   (       )
+    }else{
+        print          )
+    }
+}
+
+//Resolva o desafios nos espaços em branco
+
+/*Desafio - Daenerys é a khaleesi dos Dothraki. Juntamente com Drogon, eles vão atrás do Tyrion, para tentar dominar Westeros. Ela possui, além do seu dragãozinho, um rastreador que mede o nível de energia de qualquer ser vivo. Todos os seres com o nível menor ou igual a 8000, ela considera como se fosse um inseto. Quando passa deste valor, que foi o caso do Drogon, ela se espanta e grita “Mais de 8000”. Baseado nisso, utilize a mesma tecnologia e analise o nível de energia dos seres vivos.
+
+Entrada
+A entrada é composta por vários casos de teste. A primeira linha contém um número inteiro C relativo ao número de casos de teste. Em seguida, haverá C linhas, com um número inteiro N (100 <= N <= 100000) relativo ao nível de energia de um ser vivo.
+
+Saída
+Para cada valor lido, imprima o texto correspondente.*/
