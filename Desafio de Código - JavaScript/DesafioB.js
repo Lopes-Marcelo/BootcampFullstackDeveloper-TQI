aa45bdcf210a6fa3a7bbfa32ad70454a77e6b625
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-06-15 16:51:58 -0400

    Desafio B - Completo

--- Desafio de Código - JavaScript/DesafioB.js
+++ Desafio de Código - JavaScript/DesafioB.js
@@ -1,12 +1,9 @@
-// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
-// Abaixo segue um exemplo de código que você pode ou não utilizar
-
 const A = parseInt(gets());
 const B = parseInt(gets());
-const total =               ;
-print(total);
 
-//Escreva o seu código no espaço em branco
+let total = ((((B - A) + 1) * ( A - 1)) + ((((B - A) + 1) / 2) * ((B - A) + 1))) + (((B - A ) + 1)/2);
+
+console.log(total);
 
 /*Desafio - Um número natural é um inteiro não-negativo (0, 1, 2, 3, 4, 5,...). A sua tarefa neste problema é calcular a soma dos números naturais que estão presentes em um determinado intervalo [A, B] inclusive.
 
