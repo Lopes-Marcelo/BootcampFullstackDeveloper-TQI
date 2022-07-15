a2d888abbecfcdd4e53ae94397d7c8ea952a1816
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-15 00:21:04 -0400

    Projeto Finalizado

--- Desafio de Projeto - Java Puro e Java Spring/javaspring/src/main/java/com/javaspring/javaspring/JavaspringApplication.java
+++ Desafio de Projeto - Java Puro e Java Spring/javaspring/src/main/java/com/javaspring/javaspring/JavaspringApplication.java
@@ -2,7 +2,18 @@ package com.javaspring.javaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ */
+
+@EnableFeignClients
 @SpringBootApplication
 public class JavaspringApplication {
 
---
+++ Desafio de Projeto - Java Puro e Java Spring/javaspring/src/main/java/com/javaspring/javaspring/service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.javaspring.javaspring.service;
+
+import com.javaspring.javaspring.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ */
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
