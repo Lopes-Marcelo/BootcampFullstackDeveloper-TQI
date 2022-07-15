c2400503d8a81c886ee9f9b0ace681eb3930d784
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-15 15:18:40 -0400

    Projeto Finalizado

---
+++ Desafio de Projeto - Java Spring/javaspring/src/main/java/com/javaspring/javaspring/JavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.javaspring.javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringApplication.class, args);
+	}
+
+}
